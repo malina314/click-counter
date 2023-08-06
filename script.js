b4a3f42c9e969835335e0a839ea7eb9fbc4a03bb
Mateusz Malinowski <malina314@op.pl>
2023-08-06 11:04:14 +0200

    Add persistence

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const add = document.getElementById('add');
 const counter = document.getElementById('counter');
 const remove = document.getElementById('remove');
 
+const clicks = JSON.parse(localStorage.getItem('clicks')) ?? [];
+// console.log(clicks);
+add.innerHTML = clicks.map((click) => `<p>${new Date(click)}</p>`).join('');
+counter.innerHTML = add.childElementCount;
+
 document.body.addEventListener('click', (event) => {
-    // console.log(event.target.id);
     if (event.target.id != 'remove') {
+        clicks.push(new Date());
+        localStorage.setItem('clicks', JSON.stringify(clicks));
         add.innerHTML += `<p>${new Date()}</p>`;
         counter.innerHTML = add.childElementCount;
     }
 });
 
 remove.addEventListener('click', (event) => {
-    // console.log(event);
+    clicks.pop();
+    localStorage.setItem('clicks', JSON.stringify(clicks));
     add.lastChild?.remove();
     counter.innerHTML = add.childElementCount;
 });
